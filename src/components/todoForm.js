e4284d51d0eb25e5b9b840ca9c8b84f2de22c07b
vothanhtinh <vothanhtinh147@gmail.com>
2023-07-04 18:18:11 +0700

    test

--- src/components/todoForm.js
+++ src/components/todoForm.js
@@ -1,17 +1,22 @@
-// src/components/TodoForm.js
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { addToDo } from '../features/toDo/todoSlice';
+import { useDispatch,useSelector } from 'react-redux';
+import { addToDo, removeFromToDo } from '../features/toDo/todoSlice';
+import { Button } from '@antscorp/antsomi-ui';
 
 const ToDo = () => {
   const [text, setText] = useState('');
   const dispatch = useDispatch();
+  const  todos= useSelector(state =>state.todos)
 
   const handleSubmit = () => {
-  //  if (text.trim() !== '') {
-      ;
- //     setText('');
-  //  }
+    if (text.trim() !== '') {
+      dispatch(addToDo(text));
+      setText('');
+    }
+  };
+
+  const handleRemove = (id) => {
+    dispatch(removeFromToDo(id));
   };
 
   return (
@@ -21,8 +26,29 @@ const ToDo = () => {
         value={text}
         onChange={e => setText(e.target.value)}
       />
-      <button onClick={()=>dispatch(addToDo())}>Thêm</button>
-      </>
+      <button onClick={()=>handleSubmit()}>Thêm</button>
+
+      {/* Render the todos */}
+      <ul>
+        {todos.map(todo => (
+          
+          <div key={todo.id} style={{display:'flex', marginRight: '20px'}}>
+            <li >{todo.text}</li>
+            <Button 
+              onClick={()=>handleRemove(todo.id)}
+              danger
+              htmlType="button"
+              shape="default"
+              size="middle"
+              type="default"
+            >
+            Xóa
+            </Button>
+          </div>
+        ))}
+      </ul>
+    
+    </>
   );
 };
 
